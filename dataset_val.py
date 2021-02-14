bca8afd4b6f1e32dbdbce9ea354a2ca178e2bae3
WuYuanhun <30903332+WuYuanhun@users.noreply.github.com>
2021-02-14 03:23:04 +0000

    add dataset_val

--- dataset_val.py
+++ dataset_val.py
@@ -1,11 +1,11 @@
 import os
 import random
 
-train_dataset_path = "dataset/images/train"
-val_dataset_path = "dataset/images/val"
+train_dataset_path = "./dataset/images/train"
+val_dataset_path = "./dataset/images/val"
 
-train_label_path = "dataset/labels/train"
-train_val_path = "dataset/labels/val"
+train_label_path = "./dataset/labels/train"
+val_label_path = "./dataset/labels/val"
 
 if __name__ == '__main__':
     filenames = os.listdir(train_label_path)
@@ -15,4 +15,4 @@ if __name__ == '__main__':
 
     for filename in val_filenames:
         os.rename(os.path.join(train_dataset_path, filename + ".jpeg"), os.path.join(val_dataset_path, filename + ".jpeg"))
-        os.rename(os.path.join(train_dataset_path, filename + ".txt"), os.path.join(val_dataset_path, filename + ".txt"))
+        os.rename(os.path.join(train_label_path, filename + ".txt"), os.path.join(val_label_path, filename + ".txt"))
