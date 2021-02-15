60207007728bb58a0c6193b8097f73dd6fd50cb7
WuYuanhun <30903332+WuYuanhun@users.noreply.github.com>
2021-02-15 20:08:15 +0000

    data_preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -63,23 +63,32 @@ def calc_IoU(box_a, box_b):
 def nms_sub(args):
     (rest_data_id_list, rest_data, offset, batch) = args
     comp_data = pd.DataFrame()
+    print(len(rest_data_id_list))
     rest_data_id_list = [rest_data_id_list[i] for i in range(offset, len(rest_data_id_list), batch)]
+    print(len(rest_data_id_list))
     for nid in tqdm(rest_data_id_list):
+        if "c2d4ab1622ab34e5680710a63501c8b6" in nid:
+            print("it;s me")
         kp = rest_data[rest_data.nid == nid].reset_index(drop = True)
-        nkp = len(kp)
+
+        # comp_data = comp_data.append(kp, ignore_index=True)
+        # continue
+
+        nkp = kp.image_id.size
         if nkp == 1:
             kp.pop('nid')
-            comp_data.append(kp)
+            comp_data = comp_data.append(kp)
             continue
         contain_list = [True] * nkp
         for i in range(nkp - 1):
             if contain_list[i]:
                 box_i = box(kp.loc[i])
-            for j in range(i + 1, nkp):
-                if contain_list[j]:
-                    if calc_IoU(box_i, box(kp.loc[j])) > 0.3:
-                        contain_list[j] = False
-        kp["exist"] = contain_list
+                for j in range(i + 1, nkp):
+                    if contain_list[j]:
+                        if True or calc_IoU(box_i, box(kp.loc[j])) > 0.3:
+                            contain_list[j] = False
+
+        kp["exist"] = pd.Series(contain_list)
         kp = kp[kp.exist == True]
         kp.pop("exist")
         kp.pop('nid')
@@ -98,7 +107,9 @@ if __name__ == "__main__":
     comp_data = train_data[train_data.class_id == 14]
     rest_data = train_data[train_data.class_id != 14]
 
-    rest_data = rest_data.assign(nid=lambda x: x.image_id + x.class_id.astype("str"))
+    print(comp_data.image_id.unique().size + rest_data.image_id.unique().size)
+
+    rest_data = rest_data.assign(nid=lambda x: x.image_id + ":" + x.class_id.astype("str"))
     rest_data_id_list = rest_data["nid"].unique()
 
     train_size = extract_image_size()
@@ -112,11 +123,23 @@ if __name__ == "__main__":
     comp_data = comp_data.drop_duplicates()
 
     # carring on nms
-    pool = mp.Pool(mp.cpu_count())
-    nmsed_datas = pool.map(nms_sub, [(rest_data_id_list, rest_data, offset, mp.cpu_count()) for offset in range(mp.cpu_count())])
+    # pool = mp.Pool(mp.cpu_count())
+    # nmsed_datas = pool.map(nms_sub, [(rest_data_id_list, rest_data, offset, mp.cpu_count()) for offset in range(mp.cpu_count())])
+
+    # pool = mp.Pool(1)
+    # nmsed_datas = pool.map(nms_sub, [(rest_data_id_list, rest_data, offset, 1) for offset in range(1)])
+    nmsed_datas = [nms_sub((rest_data_id_list, rest_data, 0, 1))]
+
+    cnt = comp_data.image_id.unique().size
 
     for nmsed_data in nmsed_datas:
         # nmsed_data.apply()
+        # cnt += nmsed_data.image_id.unique().size
+        print(nmsed_data.image_id.unique().size)
         comp_data = comp_data.append(nmsed_data, ignore_index=True)
 
+    print(comp_data.image_id.unique().size)
+
+
     comp_data.to_csv(os.path.join(EXP_PATH, "train_set.csv"))
+    print("complete")
